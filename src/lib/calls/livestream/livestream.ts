3826086f1f733a6b43861f580e720f16dbc3f1fa
Max R <max8rr8@gmail.com>
2024-01-21 09:25:12 +0300

    Start implementing stream access

--- src/lib/calls/livestream/livestream.ts
+++ src/lib/calls/livestream/livestream.ts
@@ -1,8 +1,50 @@
 import EventListenerBase from '../../../helpers/eventListenerBase';
 import pause from '../../../helpers/schedulers/pause';
-import {GroupCall, InputGroupCall} from '../../../layer';
+import {GroupCall, GroupCallStreamChannel, InputGroupCall} from '../../../layer';
 import {AppManagers} from '../../appManagers/managers';
 import rootScope from '../../rootScope';
+
+
+class LiveStreamSource {
+  public current_chunk: number;
+
+  constructor(private managers: AppManagers, public stream: LiveStream, private channels: GroupCallStreamChannel[]) {
+    console.log('STREAM Started source', channels)
+    this.current_chunk = 0;
+    this.managers = rootScope.managers
+  }
+
+  async run() {
+    const dcId = (this.stream.groupCallFull._ == 'groupCall') ? this.stream.groupCallFull.stream_dc_id : 0;
+
+    const channel = 1
+    const quality = 2
+    const last_time = Number(this.channels[channel].last_timestamp_ms);
+
+    while(!this.stream.closed) {
+      await pause(1000);
+      // const download = await this.managers.apiFileManager.download({
+      //   location: {
+      //     _: 'inputGroupCallStream',
+      //     call: this.stream.groupCall,
+      //     scale: 0, // 1000ms
+      //     time_ms: last_time + this.current_chunk * 1000,
+      //     video_quality: quality,
+      //     video_channel: channel
+      //     // flags:
+      //   },
+      //   dcId
+      // })
+
+      // // Lemme imagine this is real
+      // // this.isLive = true;
+
+      // console.log('STREAM DOWNLOAD', download)
+      // const arbuf = await download.arrayBuffer();
+      // console.log( 'stream buf', arbuf)
+    }
+  }
+}
 
 export class LiveStream extends EventListenerBase<{
   closed: ()=>void
@@ -11,10 +53,12 @@ export class LiveStream extends EventListenerBase<{
   protected managers: AppManagers
   public groupCall: InputGroupCall
   public groupCallFull: GroupCall
+  public closed: boolean;
   private fullUpdaterInterval: number
 
   constructor(public peerId: PeerId, call?: InputGroupCall) {
     super()
+    this.closed = false;
     this.managers = rootScope.managers
     if(call) {
       this.groupCall = call
@@ -25,6 +69,7 @@ export class LiveStream extends EventListenerBase<{
         access_hash: ''
       }
     }
+    this.addEventListener('closed', () => this.closed = true, {once: true})
   }
 
   async join() {
@@ -86,41 +131,24 @@ export class LiveStream extends EventListenerBase<{
   }
 
   async streamIntoVideo() {
-    const channels = await this.managers.appGroupCallsManager.getStreamChannels(this.groupCall);
-    console.log('AAAAAA', channels)
+    let fail = false
+    while(!this.closed) {
+      if(fail) await pause(500);
+      const channels: GroupCallStreamChannel[] = await this.managers.appGroupCallsManager.getStreamChannels(this.groupCall);
+      if(channels.length == 0) {
+        fail = true;
+        continue
+      }
 
+      fail = false;
 
-    const fullCall = await this.managers.appGroupCallsManager.getGroupCallFull(this.groupCall.id);
-    const dcId = (fullCall._ == 'groupCall') ? fullCall.stream_dc_id : 0;
-
-    const channel = 1
-    const quality = 2
-    const last_time = Number(channels[channel].last_timestamp_ms);
-
-    // CHANGE THERE TO DOWNLOAD CHUNKS
-    for(let i = 0; i < 0; i++) {
-      await pause(1000);
-      const download = await this.managers.apiFileManager.download({
-        location: {
-          _: 'inputGroupCallStream',
-          call: this.groupCall,
-          scale: 0, // 1000ms
-          time_ms: last_time + i * 1000,
-          video_quality: quality,
-          video_channel: channel
-          // flags:
-        },
-        dcId
-      })
-
-      // Lemme imagine this is real
-      // this.isLive = true;
-
-      console.log('STREAM DOWNLOAD', download)
-      const arbuf = await download.arrayBuffer();
-      console.log('stream buf', arbuf)
-      const baseenc = btoa(new Uint8Array(arbuf).reduce((data, byte) => data + String.fromCharCode(byte), ''));
-      console.log('stream chunk', baseenc);
+      const source = new LiveStreamSource(this.managers, this, channels)
+      try {
+        await source.run()
+      } catch(e) {
+        console.error('LiveStream source failed', e)
+        fail = true
+      }
     }
   }
 }
